e28c0311a078936c4953271db71fec5d989fef2f
Hritik Jain <31235739+CodeMostt@users.noreply.github.com>
2019-12-20 11:57:10 +0530

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,54 @@
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+#step 3 - flatten
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+
+classifier.add(Dense(128, activation = 'relu'))
+classifier.add(Dense(15, activation='softmax'))
+
+# Compiling the CNN
+classifier.compile(optimizer='rmsprop',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('plantdisease/train_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('plantdisease/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 17397,
+                         epochs = 10,
+                         validation_data = test_set,
+                         validation_steps = 4119)
